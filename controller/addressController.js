e3301ede5b0b23be4f16f569a90901e48c087f9e
clotilde98 <clotildesemeha@gmail.com>
2025-10-30 16:09:03 +0100

    adress change

--- controller/addressController.js
+++ controller/addressController.js
@@ -2,14 +2,14 @@ import { pool } from '../database/database.js';
 import * as addressModel from '../model/addressDB.js';
 
 
-export const getAddressesByUser = async (req, res) => {
+export const getAddressByUser = async (req, res) => {
   try {
     const id = parseInt(req.params.id, 10);
     if (Number.isNaN(user_id)) {
       return res.status(400).json({ message: 'user_id invalide' });
     }
 
-    const addresses = await addressModel.getAddressesByUser(pool, id);
+    const addresses = await addressModel.getAddressByUser(pool, id);
     res.json(addresses);
   } catch (err) {
     console.error('getAddressesByUser error:', err);
